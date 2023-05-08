8d45490620d06a9a3056af0553d22357031032c2
Jonathan Sheely <jsheely@thirtytech.net>
2023-05-08 08:54:04 -0400

    Initial

---
+++ BlazorSSR1/Server/Pages/ServerPre.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorSSR1.Server.Pages
+{
+    public class ServerPreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
